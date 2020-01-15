1d8abcd62ca3e1bf86eb01108b6987395e72533f
bublik77 <andrii.bobrovych@gmail.com>
2020-01-15 19:13:53 +0200

    add try to get page

--- ssl_check.sh
+++ ssl_check.sh
@@ -22,17 +22,22 @@ send_report(){
 }
 
 check_date() {
-	DATE_TODAY=$(date -d "$(date '+%b %d %Y')" +%s)
-	DAY_EXPIERED=$(echo | openssl s_client -servername $1 -connect $(host $1 | head -1 | cut -d" " -f4):443 2>/dev/null | openssl x509 -noout -dates | tail -1 | cut -d"=" -f2 | awk '{print $1" "$2}' | date -d "$(xargs)" +%s)
-	DAY_LEFT=$(echo $DAY_EXPIERED - $DATE_TODAY | bc | date --date=@$(xargs) +'%m,%d ' | cut -d"," -f2 | echo "$(xargs) * 1" | bc) # multiply by one to know that is a number =)
-	MONTH_LEFT=$(echo $DAY_EXPIERED - $DATE_TODAY | bc | date --date=@$(xargs) +'%m,%d ' | cut -d"," -f1 | echo "$(xargs) - 1" | bc) #I subtract 1 in order to do not count present month
-	if [[ $MONTH_LEFT -eq 1 ]] && [[ $DAY_LEFT -le 5 ]]
+	DOMAIN_STATUS=$(curl -I -sS https://$1 2>/dev/null | head -1)
+	if [[ -n $DOMAIN_STATUS ]]
 	then
-		echo -e "It looks SSL for domain $1 is ended soon, is left $MONTH_LEFT month and $DAY_LEFT days\n" >> $STATUS_FILE
-	elif [[ $MONTH_LEFT -eq 0 ]]
-	then
-		echo -e "It looks SSL for domain $1 is ended very soon, $DAY_LEFT days\n" >> $STATUS_FILE
-	fi
+		DATE_TODAY=$(date -d "$(date '+%b %d %Y')" +%s)
+		DAY_EXPIERED=$(echo | openssl s_client -servername $1 -connect $(host $1 | head -1 | cut -d" " -f4):443 2>/dev/null | openssl x509 -noout -dates | tail -1 | cut -d"=" -f2 | awk '{print $1" "$2}' | date -d "$(xargs)" +%s)
+		DAY_LEFT=$(echo $DAY_EXPIERED - $DATE_TODAY | bc | date --date=@$(xargs) +'%m,%d ' | cut -d"," -f2 | echo "$(xargs) * 1" | bc) # multiply by one to know that is a number =)
+		MONTH_LEFT=$(echo $DAY_EXPIERED - $DATE_TODAY | bc | date --date=@$(xargs) +'%m,%d ' | cut -d"," -f1 | echo "$(xargs) - 1" | bc) #I subtract 1 in order to do not count present month
+		if [[ $MONTH_LEFT -eq 1 ]] && [[ $DAY_LEFT -le 5 ]]
+		then
+			echo -e "It looks SSL for domain $1 is ended soon, is left $MONTH_LEFT month and $DAY_LEFT days\n" >> $STATUS_FILE
+		elif [[ $MONTH_LEFT -eq 0 ]]
+		then
+			echo -e "It looks SSL for domain $1 is ended very soon, $DAY_LEFT days\n" >> $STATUS_FILE
+		fi
+	else
+		echo -e "It looks like $1 has not the  certificate or it is ended"
 }
 
 while read line
